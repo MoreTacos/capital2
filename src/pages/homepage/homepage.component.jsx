a376bf8412aa4c7835c52ce7c900ffb86a9fa37e
Davide Radaelli <daviderad@outlook.com>
2020-03-01 14:55:49 -0500

    added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className="homepage">
-    <div>Homepage</div>
+    <div className="menu-item">
+      <div className="background" />
+      <Link className="game1" to="/game1">
+        Game1
+      </Link>
+    </div>
   </div>
 );
 
